5d7a07b565be0864026e48fd8d624ff610335a00
cyberweb8 <abdumalikshareef@gmail.com>
2022-12-26 05:19:41 +0500

    feat: add parseCurrentWeather

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import "./style.css";
+import { getWeather } from "./weather";
+
+getWeather(41, 69, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+  (data) => {
+    console.log(data);
+  }
+);
